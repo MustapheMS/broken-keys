91fd8dcb4de3c533faedd996abdff06ce1c5606a
MustapheMS <moztaphem9@gmail.com>
2023-06-16 13:26:02 +0300

    Broken Keys Done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+
+// Done 💯💯
+function brokenKeys(Sax, khalad) {
+    let brokenKeys = [];
+    for (let i = 0; i < Sax.length; i++) {
+      if (Sax[i] !== khalad[i]) {
+        if (!brokenKeys.includes(Sax[i])) {
+          brokenKeys.push(Sax[i]);
+        }
+      }
+    }
+    return brokenKeys;
+  }
+  console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); 
+  console.log(brokenKeys("Web Developer", "Web Devel"))
